1b0dcef3acf7bf6df98b920343d714fce8525c54
YuiAstin <48943936+YuiAstin@users.noreply.github.com>
2019-04-01 20:45:52 +0700

    update emailsender

--- EmailSender.cs
+++ EmailSender.cs
@@ -11,21 +11,21 @@ namespace BUS.Helpers
 	{
 		public static void SendMessage(string email, string message)
 		{
-            SmtpClient smtpClient = new SmtpClient("mail.hostdomain.com", 25); //reconfig mail host vd: mail.google.com
+			SmtpClient smtpClient = new SmtpClient("mail.hostdomain.com", 25); //reconfig mail host vd: mail.google.com
 
-            smtpClient.Credentials = new System.Net.NetworkCredential("admin@mail.com", "IDpassword"); //reconfig ID and password 
-            smtpClient.UseDefaultCredentials = true;
-            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
-            smtpClient.EnableSsl = true;
+			smtpClient.Credentials = new System.Net.NetworkCredential("admin@mail.com", "IDpassword"); //reconfig ID and password 
+			smtpClient.UseDefaultCredentials = true;
+			smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+			smtpClient.EnableSsl = true;
 
-            MailMessage mail = new MailMessage();
-            mail.Body = message;
+			MailMessage mail = new MailMessage();
+			mail.Body = message;
 
-            //Setting From , To and CC if needed
-            mail.From = new MailAddress("admin@mail.com", "MyWeb Site"); //reconfig ID, displayed name can be reconfigure but not needed
-            mail.To.Add(new MailAddress(email));
-            //mail.CC.Add(new MailAddress("thu3@mail.com"));
-            smtpClient.Send(mail);
-        }
+			//Setting From , To and CC if needed
+			mail.From = new MailAddress("admin@mail.com", "MyWeb Site"); //reconfig ID, displayed name can be reconfigure but not needed
+			mail.To.Add(new MailAddress(email));
+			//mail.CC.Add(new MailAddress("thu3@mail.com"));
+			smtpClient.Send(mail);
+		}
 	}
 }
